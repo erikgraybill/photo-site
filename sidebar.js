324b4eef910d1aa1ea364cada1a4b57c0b8a3e49
Erik <graybiller@gmail.com>
2026-01-29 12:38:40 -0500

    initial commit

---
+++ sidebar.js
@@ -0,0 +1,36 @@
+const openButton = document.getElementById('open-sidebar-button')
+const navbar = document.getElementById('navbar')
+
+const media = window.matchMedia("(width < 700px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e) {
+    if (e.matches) {
+        navbar.setAttribute('inert', '')
+    }
+    else {
+        navbar.removeAttribute('inert')
+    }
+}
+
+function openSidebar() {
+    navbar.classList.add('show')
+    openButton.setAttribute('aria-expanded', 'true')
+    navbar.removeAttribute('inert')
+}
+
+function closeSidebar() {
+    navbar.classList.remove('show')
+    openButton.setAttribute('aria-expanded', 'false')
+    navbar.setAttribute('inert', '')
+}
+
+const navLinks = document.querySelectorAll('nav a')
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        closeSidebar()
+    })
+})
+
+updateNavbar(media)
