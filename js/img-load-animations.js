a9126c421e00e40352026217669bb245dd48d44f
Erik <graybiller@gmail.com>
2026-04-21 16:11:53 -0400

    -finish converting images to webp -added load animations to images

--- js/img-load-animations.js
+++ js/img-load-animations.js
@@ -5,14 +5,20 @@ images.forEach((img, i) => {
 });
 
 const observer = new IntersectionObserver((entries) => {
-entries.forEach(entry => {
-    if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target); // animate only once
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            
+            //Enable hover animation AFTER reveal is done
+            setTimeout(() => {
+                entry.target.classList.add('test');
+            }, 125 * images.length); //related to CSS transition duration, in ms?
+            
+            observer.unobserve(entry.target); // animate only once
         }
     });
 }, {
-    threshold: 0.2 // triggers when 20% of the image is visible
+    threshold: 0.2 //triggers when 20% of the image is visible
 });
 
 images.forEach(img => observer.observe(img));
